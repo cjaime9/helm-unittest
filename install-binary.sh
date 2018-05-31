fcba787a2775885eca7eabc2ea6bf1a906a87188
Chris Jaime <christopher.jaime@tenmarks.com>
2018-05-31 15:56:48 -0700

    Update install-binary.sh

--- install-binary.sh
+++ install-binary.sh
@@ -106,7 +106,7 @@ installFile() {
   echo "Preparing to install into ${HELM_UNIT}"
   echo "GOING TO DO A COPY FOR ${HELM_TMP_BIN} to ${HELM_UNIT}"
   # Use * to also copy the file withe the exe suffix on Windows
-  cp -rF "$HELM_TMP" "$HELM_UNIT"
+  cp -rf "$HELM_TMP" "$HELM_UNIT"
   echo "Print out contents of ${HELM_UNIT}"
   echo $(ls $HELM_UNIT)
   echo "Print out contents of ${HELM_TMP}"
