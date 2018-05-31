0cf7962a2750b2caa552749e3eb31bb6d4c45d92
Chris Jaime <christopher.jaime@tenmarks.com>
2018-05-31 14:20:32 -0700

    Update install-binary.sh

--- install-binary.sh
+++ install-binary.sh
@@ -100,13 +100,13 @@ installFile() {
   mkdir -p "$HELM_TMP"
   tar xf "$PLUGIN_TMP_FILE" -C "$HELM_TMP"
   HELM_TMP_BIN="$HELM_TMP/untt"
-  echo "CREATE DIRECTOR ${HELM_PLUGIN_PATH}/helm-unittest"
-  mkdir -p "${HELM_PLUGIN_PATH}/helm-unittest"
+  echo "CREATE DIRECTORY ${HELM_PLUGIN_PATH}"/helm-unittest
+  mkdir -p "${HELM_PLUGIN_PATH}"/helm-unittest
   echo "Preparing to install into ${HELM_PLUGIN_PATH}"
   echo "GOING TO DO A COPY FOR ${HELM_TMP_BIN} to ${HELM_PLUGIN_PATH}"
   # Use * to also copy the file withe the exe suffix on Windows
   cp "$HELM_TMP_BIN" "$HELM_PLUGIN_PATH"
-  ECHO "DONE WIHT THE COPY"
+  echo "DONE WIHT THE COPY"
 }
 
 # fail_trap is executed if an error occurs.
