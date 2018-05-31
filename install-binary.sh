31f005c99bd3af6b43856cc7ad9c493314ebc821
Chris Jaime <christopher.jaime@tenmarks.com>
2018-05-31 15:46:21 -0700

    Update install-binary.sh

--- install-binary.sh
+++ install-binary.sh
@@ -108,7 +108,7 @@ installFile() {
   # Use * to also copy the file withe the exe suffix on Windows
   cp "$HELM_TMP_BIN" "$HELM_UNIT"
   echo "Print out contents of ${HELM_UNIT}"
-  echo $(ls /home/jenkins/.helm)
+  echo $(ls $HELM_UNIT)
   echo "DONE WIHT THE COPY"
 }
 
