3f8437213c005d9e1f52c97b7a3ba02ac92e1a4b
Chris Jaime <christopher.jaime@tenmarks.com>
2018-06-03 11:24:38 -0700

    Update untt.sh

--- untt.sh
+++ untt.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-echo 'hello world'
+echo "First arg: $1"
+echo "hell world 1"
+echo "Second arg: $2"
+echo "ehll world 2"
+echo "List of all arg: $@"
+echo "hell world 3"
+/home/jenkins/.helm/cache/plugins/https-github.com-cjaime9-helm-unittest/untt $@
